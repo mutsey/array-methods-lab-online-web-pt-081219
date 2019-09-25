d308ef87118317b8da3ba7595f57cf2576c65373
Meagan Utsey <meagan.utsey@gmail.com>
2019-09-25 23:03:53 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,7 +7,9 @@ famous_cats.include?("Maru")
 end
 
 def using_sort(array)
-
+famous_cats = ["lil' bub", "grumpy cat", "maru"]
+famous_cats.sort
+  => ["grumpy cat", "lil' bub", "maru"]
 end
 
 def using_reverse(array)
