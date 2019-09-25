52a8e1fbebad49dc29b772b3a85721953c7cea0c
Meagan Utsey <meagan.utsey@gmail.com>
2019-09-25 23:04:20 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -13,7 +13,9 @@ famous_cats.sort
 end
 
 def using_reverse(array)
-
+famous_wizards = ["Dumbledore", "Gandalf", "Merlin"]
+famous_wizards.reverse
+  => ["Merlin", "Gandalf", "Dumbledore"]
 end
 
 def using_first(array)
